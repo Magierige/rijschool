2ca51f5f176ec5537c6163a1cb7122a8118ec005
Magierige <r.vandevelde01@student.alfa-college.nl>
2024-03-05 10:06:34 +0100

    added relation package
    heb een composer paket geinstaleerd voor de permisies

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->date('birthdate');
-            $table->string('place');
+            $table->string('street');
+            $table->string('city');
+            $table->string('house_number');
+            $table->string('zip');
+            $table->string('country');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- database/migrations/2024_03_04_161609_add_relations_to_user.php
+++ database/migrations/2024_03_04_161609_add_relations_to_user.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('plan_id')->nullable();
-            $table->unsignedBigInteger('role_id')->nullable();
             
             $table->foreign('plan_id')->references('id')->on('plans');
-            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
